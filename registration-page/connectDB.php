0fa653297d93b2ba4eabedb3e711894070cace5d
eric-ha <eric.ha006@gmail.com>
2017-04-01 19:38:45 -0600

    Added database name

--- registration-page/connectDB.php
+++ registration-page/connectDB.php
@@ -3,7 +3,7 @@
 	$server = "us-cdbr-iron-east-03.cleardb.net";
 	$username = "b93aa055892ff0";
 	$password = "a2da8580";
-	define('DBNAME', 'users');
+	define('DBNAME', 'heroku_2dba9b3d9ee490e');
 
 	$conn = new mysqli($server, $username, $password, DBNAME);
 
