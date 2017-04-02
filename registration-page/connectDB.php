45f2c084ea35f45b49376663945d8d4859533f37
eric-ha <eric.ha006@gmail.com>
2017-04-01 19:33:54 -0600

    Fixing errors

--- registration-page/connectDB.php
+++ registration-page/connectDB.php
@@ -1,12 +1,12 @@
 <?php
 
 	$url = parse_url(getenv("mysql://b93aa055892ff0:a2da8580@us-cdbr-iron-east-03.cleardb.net/heroku_2dba9b3d9ee490e?reconnect=true"));
-	$server = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr($url["path"], 1);
+	$server = $url["us-cdbr-iron-east-03.cleardb.net"];
+	$username = $url["b93aa055892ff0"];
+	$password = $url["a2da8580"];
+	define('DBNAME', 'users');
 
-	$conn = new mysqli($server, $username, $password, $db);
+	$conn = new mysqli($server, $username, $password, DBNAME);
 
 	if(!$conn){
 		die("connection failed : " . mysql_error());
