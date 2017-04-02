7d571281e71f9686f0e67d12f016efd145be602f
eric-ha <eric.ha006@gmail.com>
2017-04-01 19:21:16 -0600

    Trying to connect clearDB with PHP

--- registration-page/connectDB.php
+++ registration-page/connectDB.php
@@ -1,20 +1,12 @@
 <?php
 
-	errror_reporting( ~E_DEPRECATED & ~E_NOTICE );
-	define('DBHOST', 'us-cdbr-iron-east-03.cleardb.net');
-	define('DBUSER', 'b93aa055892ff0');
-	define('DBPASS', 'a2da8580');
-	define('DBNAME', 'users');
+	$url = parse_url(getenv("mysql://b93aa055892ff0:a2da8580@us-cdbr-iron-east-03.cleardb.net/heroku_2dba9b3d9ee490e?reconnect=true"));
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 
-	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+	$conn = new mysqli($server, $username, $password, $db);
  	$dbcon = mysql_select_db(DBNAME);
- 
- 	if ( !$conn ) {
-  		die("Connection failed : " . mysql_error());
- 	}
- 
- 	if ( !$dbcon ) {
-  		die("Database Connection failed : " . mysql_error());
-	}
+
 ?>
-	
