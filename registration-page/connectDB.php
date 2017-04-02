a6a7445d36132c58d2e1776b5abaa0e7aee8a9e4
eric-ha <eric.ha006@gmail.com>
2017-04-01 19:37:18 -0600

    Fix

--- registration-page/connectDB.php
+++ registration-page/connectDB.php
@@ -1,9 +1,8 @@
 <?php
 
-	$url = parse_url(getenv("mysql://b93aa055892ff0:a2da8580@us-cdbr-iron-east-03.cleardb.net/heroku_2dba9b3d9ee490e?reconnect=true"));
-	$server = $url["us-cdbr-iron-east-03.cleardb.net"];
-	$username = $url["b93aa055892ff0"];
-	$password = $url["a2da8580"];
+	$server = "us-cdbr-iron-east-03.cleardb.net";
+	$username = "b93aa055892ff0";
+	$password = "a2da8580";
 	define('DBNAME', 'users');
 
 	$conn = new mysqli($server, $username, $password, DBNAME);
