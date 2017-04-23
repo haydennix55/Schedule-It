8eff46060a8a2f3ae3d601239286519f6b1edc7c
haydennix55 <hani2284@colorado.edu>
2017-04-23 15:16:26 -0600

    Link buttons

--- registration-page/registration-complete.php
+++ registration-page/registration-complete.php
@@ -45,7 +45,17 @@
  <link rel="stylesheet" type="text/css" href="registration-complete.css" media="screen" />
     <body>
         <div>
-            <h1>Thanks for Registering!</h1>
+            <h1 class="message">Thanks for Registering!</h1>
         </div>
+        <div class="buttons">
+            <form action="http://schedule-it.herokuapp.com/user-dashboard/index.html">
+                <button type="submit" class="linkButton">Continue to Dashboard</button>
+            </form>
+            <form action="http://schedule-it.herokuapp.com/login-page/index.php">
+                <button type="submit" class="linkButton">Login Page</button>
+            </form>
+
+        </div>
+
     </body>
  </html>
