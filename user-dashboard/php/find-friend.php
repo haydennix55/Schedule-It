54559b121fc223bdd13b5c0775773b537cac1435
haydennix55 <hani2284@colorado.edu>
2017-04-25 07:57:08 -0600

    find-friend

---
+++ user-dashboard/php/find-friend.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])) {
+		include_once '../include/connectDB.php';
+
+		$searhInput = mysqli_real_escape_string($conn, $_POST['Search']);
+
+		$sql = "SELECT first_name, last_name, username from users where username = ". $searchInput;
+
+
+		$query = mysqli_query($conn, $sql);
+
+
+		while ($row = mysqli_fetch_assoc($query)){
+			echo "Match Found"
+		}
+	}
+
+ ?>
