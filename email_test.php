4179022da2116fd03151858a8b933d97ee909d3f
Scott Campana <sbcampana@gmail.com>
2017-04-30 14:41:16 -0600

    changed example email test

--- email_test.php
+++ email_test.php
@@ -1,16 +1,19 @@
 <?php
-
-echo "hello";
 // using SendGrid's PHP Library
 // https://github.com/sendgrid/sendgrid-php
+// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-$sendgrid = new SendGrid("SENDGRID_APIKEY");
-$email    = new SendGrid\Email();
-
-$email->addTo("test@sendgrid.com")
-      ->setFrom("you@youremail.com")
-      ->setSubject("Sending with SendGrid is Fun")
-      ->setHtml("and easy to do anywhere, even with PHP");
-
-$sendgrid->send($email);
+// If you are not using Composer
+// require("path/to/sendgrid-php/sendgrid-php.php");
+$from = new SendGrid\Email("Example User", "test@example.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "test@example.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
  ?>
