8a1c4142fe2cca8cffc327ca1c7c93ef0769fcc7
haydennix55 <hani2284@colorado.edu>
2017-04-30 13:35:48 -0600

    removal of message

--- user-dashboard/calender.php
+++ user-dashboard/calender.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
-    $message = "Sorry, you must be logged in to view this page!";
-    echo "<script type='text/javascript'>alert('$message');</script>"
     header("Location:/login-page/index.php");
 }
 
