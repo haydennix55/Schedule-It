2f335fd7efa0d29f7a6f7543abe62f9723a5e895
Scott Campana <sbcampana@gmail.com>
2017-04-30 13:17:25 -0600

    added test email

--- registration-page/registration-complete.php
+++ registration-page/registration-complete.php
@@ -37,6 +37,23 @@
 		else {
 			echo "The two passwords you typed did not match";
 		}
+
+		require_once 'vendor/autoload.php';
+
+		$from = new SendGrid\Email(null, "test@example.com");
+		$subject = "Hello World from the SendGrid PHP Library!";
+		$to = new SendGrid\Email(null, $email);
+		$content = new SendGrid\Content("text/plain", "Hello, Email");
+		$email = new SendGrid\Mail($from, $subject, $to, $content);
+
+		$apiKey = getenv('SENDGRID_API_KEY');
+		$sg = new \SendGrid($apiKey);
+
+		$response = $sg->client->mail()->send()->post($mail);
+		echo $response->statusCode();
+		echo $response->headers();
+		echo $response->body();
+
 	}
 
  ?>
