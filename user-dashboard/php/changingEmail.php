ead09bf1d43ad88dba159bd8abda0cbad7440f78
haydennix55 <hani2284@colorado.edu>
2017-05-02 19:44:45 -0600

    query

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,23 +1,17 @@
 <?php
     session_start();
 
-        $server = "us-cdbr-iron-east-03.cleardb.net";
-    	$username = "b93aa055892ff0";
-    	$password = "a2da8580";
-    	define('DBNAME', 'heroku_2dba9b3d9ee490e');
+    include "/include/connectDB.php";
 
-    	$conn = new mysqli($server, $username, $password, DBNAME);
+    $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
+    $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
+    $sql = 'UPDATE users SET email = "' . $newEmail . '" WHERE email = "' . $currentEmail .'"';
 
-        $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
-        $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
-        $password = mysqli_real_escape_string($conn, $_POST['password']);
+    echo $sql;
 
-        $sql = "UPDATE users SET email = 'hani2284@colorado.edu' WHERE email = 'hayden.nix@colorado.edu'";
-
-        echo $sql;
-
-        $query = mysqli_query($conn, $sql);
+    $query = mysqli_query($conn, $sql);
 
 
     header("Location:../index.php");
