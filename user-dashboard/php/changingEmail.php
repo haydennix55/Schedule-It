2cd162568d872a5ed90ff8c16a7d792c3583266c
haydennix55 <hani2284@colorado.edu>
2017-05-02 22:34:13 -0600

    changing include line

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,15 +1,14 @@
 <?php
     session_start();
 
-    include "/include/connectDB.php";
+    include "../connectDB.php";
 
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $sql = 'UPDATE users SET email = "' . mysql_real_escape_string($newEmail) . '" WHERE email = "' . mysql_real_escape_string($currentEmail) .'"';
+    $sql = 'UPDATE users SET email = "' . $newEmail . '" WHERE email = "' . $currentEmail .'"';
 
-    echo $sql;
 
     $query = mysqli_query($conn, $sql);
 
