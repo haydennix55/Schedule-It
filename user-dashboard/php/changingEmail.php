b47337149371d25607491b5d60e3ab2694c03d46
haydennix55 <hani2284@colorado.edu>
2017-05-02 22:41:08 -0600

    hardcode test db connect

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,7 +1,16 @@
 <?php
     session_start();
 
-    include "../connectDB.php";
+    $server = "us-cdbr-iron-east-03.cleardb.net";
+    $username = "b93aa055892ff0";
+    $password = "a2da8580";
+    define('DBNAME', 'heroku_2dba9b3d9ee490e');
+
+    $conn = new mysqli($server, $username, $password, DBNAME);
+
+    if(mysqli_ping($conn) == false){
+        echo "Connection did not work";
+    }
 
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
