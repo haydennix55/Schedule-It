fb39542b2422d09cb5e91cd9f936bda102f09ffe
haydennix55 <hani2284@colorado.edu>
2017-05-02 18:36:06 -0600

    hardcode sql statement test

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -14,7 +14,7 @@
         $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-        $sql = "UPDATE users SET email = ''" . $newEmail . "' WHERE email = '" . $currentEmail . "''";
+        $sql = "UPDATE users SET email = 'hani2284@colorado.edu' WHERE email = 'hayden.nix@colorado.edu';
 
         $query = mysqli_query($conn, $sql);
 
