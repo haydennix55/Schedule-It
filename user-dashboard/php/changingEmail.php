36c07fc8da5f1f5dc0f83dddf18d8b87c9d49a66
haydennix55 <hani2284@colorado.edu>
2017-05-02 22:43:31 -0600

    hardcode test update statement

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,22 +1,13 @@
 <?php
     session_start();
 
-    $server = "us-cdbr-iron-east-03.cleardb.net";
-    $username = "b93aa055892ff0";
-    $password = "a2da8580";
-    define('DBNAME', 'heroku_2dba9b3d9ee490e');
-
-    $conn = new mysqli($server, $username, $password, DBNAME);
-
-    if(mysqli_ping($conn) == false){
-        echo "Connection did not work";
-    }
+    include "../connectDB.php";
 
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $sql = 'UPDATE users SET email = "' . $newEmail . '" WHERE email = "' . $currentEmail .'"';
+    $sql = "UPDATE users SET email = 'hani2284@colorado.edu' WHERE email = 'hayden.nix@colorado.edu'";
 
 
     $query = mysqli_query($conn, $sql);
