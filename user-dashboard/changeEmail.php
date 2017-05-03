6c9c8c99b551ec28bbaa8784907810ccbff59e02
haydennix55 <hani2284@colorado.edu>
2017-05-03 10:40:16 -0600

    add names to inputs

--- user-dashboard/changeEmail.php
+++ user-dashboard/changeEmail.php
@@ -13,15 +13,15 @@
     <form method ="post" action="php/changingEmail.php">
         <div>
             <label>Current Email: </label></br>
-            <input class="currentEmail" type="text" placeholder="Current Email" id="currentEmail"></br>
+            <input class="currentEmail" type="text" placeholder="Current Email" name="currentEmail" id="currentEmail"></br>
 
             <label>New Email: </label></br>
-            <input class="newEmail" type="text" placeholder="New Email" id="newEmail"></br>
+            <input class="newEmail" type="text" placeholder="New Email" name="newEmail" id="newEmail"></br>
 
             <label>Password: </label></br>
-            <input class="currentPass" type="password" placeholder="Password" id="password"></br>
-            
-            <button type="submit">
+            <input class="currentPass" type="password" placeholder="Password" name="password" id="password"></br>
+
+            <button name="submit" value="submit" type="submit">
         </div>
     </form>
 
