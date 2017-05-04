3f729d7ee407f1e68299145fb0f172c7b9679275
haydennix55 <hani2284@colorado.edu>
2017-05-03 18:46:01 -0600

    check password for email change

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,17 +1,24 @@
 <?php
     session_start();
 
+    $username = $_SESSION['username'];
+
     include "../connectDB.php";
 
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $sql = "UPDATE users SET email = '" . $newEmail . "' WHERE email = '" . $currentEmail . "'";
+    $sql = "SELECT password FROM users where (email = '" . $currentEmail . "' and username =  '" . $username . "'";
 
+    $query = mysqli_query($conn,$sql);
 
-    $query = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($query);
 
+    if password_verify($password, $row['password']){
+
+        $sql = "UPDATE users SET email = '" . $newEmail . "' WHERE (email = '" . $currentEmail . "' and username = '" . $username . "'";
+        $query = mysqli_query($conn, $sql);
 
     header("Location:../index.php");
 
