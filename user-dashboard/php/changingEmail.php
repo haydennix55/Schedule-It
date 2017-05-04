477a28c2e3d84691c512eb79e06fdafab3bbf377
haydennix55 <hani2284@colorado.edu>
2017-05-03 20:49:11 -0600

    echo tests

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -3,6 +3,8 @@
 
     include "../connectDB.php";
 
+    echo "DB Connected";
+
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
     $username = $_SESSION['username'];
@@ -12,16 +14,20 @@
 
     $query = mysqli_query($conn, $sql);
 
+    echo "Query";
+
     while ($row = mysqli_fetch_assoc($query)){
 
         if($username == $row['username'] and password_verify($password, $row['password'])){
+
+            echo "Password check";
 
             $sql = "UPDATE users SET email = '" . $newEmail . "' WHERE (email = '" . $currentEmail . "' and $username = '" . $username . "'";
             $query = mysqli_query($conn, $sql);
         }
     }
 
-    header("Location:../index.php");
+    //header("Location:../index.php");
 
 
  ?>
