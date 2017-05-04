8c4d1396875cdbde8a8c0900e74a551dd83d8367
haydennix55 <hani2284@colorado.edu>
2017-05-03 20:44:26 -0600

    rewrite security logic

--- user-dashboard/php/changingEmail.php
+++ user-dashboard/php/changingEmail.php
@@ -1,24 +1,24 @@
 <?php
     session_start();
 
-    $username = $_SESSION['username'];
-
     include "../connectDB.php";
 
     $currentEmail = mysqli_real_escape_string($conn, $_POST['currentEmail']);
     $newEmail = mysqli_real_escape_string($conn, $_POST['newEmail']);
+    $username = $_SESSION['username'];
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $sql = "SELECT password FROM users where (email = '" . $currentEmail . "' and username =  '" . $username . "'";
+    $sql = "SELECT password FROM users WHERE username = '" . $username . "'";
 
-    $query = mysqli_query($conn,$sql);
+    $query = mysqli_query($conn, $sql);
 
-    $row = mysqli_fetch_assoc($query);
+    while ($row = mysqli_fetch_assoc($query)){
 
-    if (password_verify($password, $row['password'])){
+        if($username == $row['username'] and password_verify($password, $row['password'])){
 
-        $sql = "UPDATE users SET email = '" . $newEmail . "' WHERE (email = '" . $currentEmail . "' and username = '" . $username . "'";
-        $query = mysqli_query($conn, $sql);
+            $sql = "UPDATE users SET email = '" . $newEmail . "' WHERE (email = '" . $currentEmail . "' and $username = '" . $username . "'";
+            $query = mysqli_query($conn, $sql);
+        }
     }
 
     header("Location:../index.php");
